b804af5833622d0fa4c76e277567aa7e2d0815dc
Rutia08 <rutia08@gmail.com>
2022-12-04 22:15:38 +0300

    начальная разметка и стилизация

--- source/js/main.js
+++ source/js/main.js
@@ -12,7 +12,65 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   // ---------------------------------
+  // form
+  let inputName = document.querySelector('[data-name-input]');
+  let inputPhone = document.querySelector('[data-phone-input]');
 
+  inputName.oninput = () => {
+    if (inputName.value.length < 2) {
+      inputName.classList.add('is-invalid');
+    } else {
+      inputName.classList.remove('is-invalid');
+    }
+  };
+
+  let keyPressHandler1 = (evt) => {
+    if (evt.keyCode !== 43) {
+      if (evt.keyCode < 47 || evt.keyCode > 57) {
+        evt.preventDefault();
+      }
+    }
+  };
+
+  let keyPressHandler2 = (evt) => {
+    if (evt.keyCode < 47 || evt.keyCode > 57) {
+      evt.preventDefault();
+    }
+  };
+
+  inputPhone.addEventListener('keypress', keyPressHandler1);
+
+  inputPhone.oninput = () => {
+    if (inputPhone.value.length > 0) {
+      inputPhone.addEventListener('keypress', keyPressHandler2);
+      inputPhone.removeEventListener('keypress', keyPressHandler1);
+    } else {
+      inputPhone.addEventListener('keypress', keyPressHandler1);
+      inputPhone.removeEventListener('keypress', keyPressHandler2);
+    }
+
+    if (inputPhone.value[0] === '+') {
+      if (inputPhone.value.length < 2) {
+        inputPhone.classList.add('is-invalid');
+      } else {
+        inputPhone.classList.remove('is-invalid');
+      }
+    } else {
+      if (inputPhone.value.length < 1) {
+        inputPhone.classList.add('is-invalid');
+      } else {
+        inputPhone.classList.remove('is-invalid');
+      }
+    }
+  };
+
+  inputName.oninput = () => {
+    if (inputName.value.length < 2) {
+      inputName.classList.add('is-invalid');
+    } else {
+      inputName.classList.remove('is-invalid');
+    }
+  };
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,75 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+
+/* eslint-disable */
+
+// swiper couches
+const swiper1 = new Swiper('.swiper-1', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    }
+  }
+});
+
+const swiperPrev1 = document.querySelector('[data-swiper-1-prev]');
+const swiperNext1 = document.querySelector('[data-swiper-1-next]');
+
+swiperPrev1.addEventListener('click', () => {
+  swiper1.slidePrev();
+});
+
+swiperNext1.addEventListener('click', () => {
+  swiper1.slideNext();
+});
+
+// swiper reviews
+const swiper2 = new Swiper('.swiper-2', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+});
+
+const swiperPrev2 = document.querySelector('[data-swiper-2-prev]');
+const swiperNext2 = document.querySelector('[data-swiper-2-next]');
+const slides2 = document.querySelectorAll('[data-slide-swiper-2]');
+
+if (swiper2.realIndex === 0) {
+  swiperPrev1.classList.add('disabled');
+};
+
+const buttonDisable2 = () => {
+  if (swiper2.realIndex === 0) {
+    swiperPrev2.classList.add('disabled');
+  } else {
+    swiperPrev2.classList.remove('disabled');
+  };
+
+  if ((swiper2.realIndex >= slides2.length / 2)) {
+    swiperNext2.classList.add('disabled');
+  } else {
+    swiperNext2.classList.remove('disabled');
+  };
+};
+
+swiper2.on('transitionEnd', function() {
+  buttonDisable2();
+});
+
+swiperPrev2.addEventListener('click', () => {
+  swiper2.slidePrev();
+  buttonDisable2();
+});
+
+swiperNext2.addEventListener('click', () => {
+  swiper2.slideNext();
+  buttonDisable2();
+});
